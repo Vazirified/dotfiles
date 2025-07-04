039955716951bdfdf9552a55f9a011454c93e8da
AV <52087173+Vazirified@users.noreply.github.com>
2025-07-04 10:22:31 +0300

    Update init.el with use-package ivy and use-package doom-modeline and a small debugging change.

--- emacs/init.el
+++ emacs/init.el
@@ -28,11 +28,34 @@
   (package-refresh-contents))
 
 ;; This sometimes doesn't work and needs investigation and debugging!
-;; (unless (package-install-p 'use-package)
-;;  (package-install 'use-package))
+(unless (package-installed-p 'use-package)
+   (package-install 'use-package))
 
 (require 'use-package)
 (setq use-package-always-ensure t)
 
 ;; Package setup
 ;; -------------
+
+(use-package ivy
+  :diminish
+  :bind (("C-s" . swiper)
+         :map ivy-minibuffer-map
+         ("TAB" . ivy-alt-done)	
+         ("C-l" . ivy-alt-done)
+         ("C-j" . ivy-next-line)
+         ("C-k" . ivy-previous-line)
+         :map ivy-switch-buffer-map
+         ("C-k" . ivy-previous-line)
+         ("C-l" . ivy-done)
+         ("C-d" . ivy-switch-buffer-kill)
+         :map ivy-reverse-i-search-map
+         ("C-k" . ivy-previous-line)
+         ("C-d" . ivy-reverse-i-search-kill))
+  :config
+  (ivy-mode 1))
+
+(use-package doom-modeline
+  :ensure t
+  :init (doom-modeline-mode 1)
+  :custom ((doom-modeline-height 15)))
